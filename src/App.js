31fc17211dc4c828e20b25b2742f3d1b127c7c91
Praveenkum12 <praveenkum1225@gmail.com>
2023-09-11 14:31:54 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,218 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useContext, useReducer } from 'react';
+import ShoppingCart from './svg/shoppingCart';
+import data from './Data';
+import MagnifyGlass from './svg/magnifyGlass';
+
+const useItem = function () {
+  return useContext(ItemContext);
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case 'updateInput':
+      return { ...state, query: action.payload };
+    case 'productMatched':
+      return {
+        ...state,
+        matchedProducts: action.payload,
+        status: action.payload.length ? 'showItems' : 'noItems',
+      };
+    case 'clickedCart':
+      return { ...state, status: 'showCart' };
+    case 'cartAdding':
+      action.payload.finalPrice =
+        action.payload.price * action.payload.quantity;
+
+      return {
+        ...state,
+        cartArr: [action.payload, ...state.cartArr],
+        totalPrice: state.totalPrice + action.payload.finalPrice,
+      };
+    default:
+      throw new Error('Unknown Action');
+  }
+};
+
+const initialState = {
+  productList: data,
+  query: '',
+  matchedProducts: [],
+  cartArr: [],
+  status: 'initial',
+  totalPrice: 0,
+};
+const ItemContext = createContext();
 
 function App() {
+  const [
+    { productList, query, matchedProducts, status, cartArr, totalPrice },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ItemContext.Provider
+      value={{
+        productList,
+        query,
+        dispatch,
+        matchedProducts,
+        status,
+        cartArr,
+        totalPrice,
+      }}
+    >
+      <NavBar />
+      <Main />
+    </ItemContext.Provider>
+  );
+}
+
+function NavBar() {
+  const { query, dispatch, productList } = useItem();
+  const handleInputChange = function (e) {
+    dispatch({
+      type: 'updateInput',
+      payload: e.target.value,
+    });
+  };
+
+  const handleSearch = () => {
+    const matchingCategory = productList.find(
+      category => Object.keys(category)[0].toLowerCase() === query.toLowerCase()
+    );
+    const products = matchingCategory
+      ? matchingCategory[query.toLowerCase()]
+      : [];
+    dispatch({ type: 'productMatched', payload: products });
+  };
+
+  return (
+    <nav className="nav">
+      <span className="logo">ShopWave</span>
+      <input
+        type="text"
+        placeholder="Search for products, brands and more"
+        className="search-bar"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button className="search-btn" onClick={handleSearch}>
+        <MagnifyGlass /> Search
+      </button>
+      <button
+        className="cart-btn"
+        onClick={() => dispatch({ type: 'clickedCart' })}
+      >
+        <ShoppingCart />
+      </button>
+    </nav>
+  );
+}
+
+function Main() {
+  const { matchedProducts, status, cartArr, totalPrice } = useItem();
+  return (
+    <main>
+      {status === 'initial' && (
+        <div className="no-product-cover">
+          <div className="no-products">🌴 Discover Your Deserved Item 🌴</div>
+        </div>
+      )}
+      {status === 'showItems' && (
+        <section className="container">
+          <h1 className="heading-product">Products</h1>
+          <p className="para-product">Take a look at our products</p>
+          <div className="fig-container">
+            {matchedProducts.map(function (product) {
+              return <Card key={product.id} product={product} />;
+            })}
+          </div>
+        </section>
+      )}
+      {status === 'noItems' && (
+        <div className="no-product-cover">
+          <div className="no-products no-item">
+            Unfortunately, there are no items <br /> to display at the moment 🥺
+          </div>
+        </div>
+      )}
+      {status === 'showCart' && (
+        <section className="cart-container container">
+          <h1 className="heading-cart">Your Cart</h1>
+          <div className="carts-titles">
+            <p>Product</p>
+            <p>Unit price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+          </div>
+          {cartArr.map(function (product) {
+            return <CartItem key={product.id} product={product} />;
+          })}
+          <div className="total-box">
+            <p>Total</p>
+            <p className="total-price">Rs.{totalPrice} \-</p>
+          </div>
+        </section>
+      )}
+    </main>
+  );
+}
+
+function CartItem({ product }) {
+  return (
+    <div className="cart-item">
+      <p>
+        <img
+          className="sm-cart-img"
+          src={product.image}
+          alt={product.productName}
+        />
+        {product.productName}
+      </p>
+      <p>&#x20B9; {product.price}</p>
+      <p>{product.quantity}</p>
+      <p>Rs.{product.finalPrice} \-</p>
     </div>
+  );
+}
+
+function Card({ product }) {
+  const { dispatch } = useItem();
+
+  function handleAddToCart() {
+    dispatch({
+      type: 'cartAdding',
+      payload: product,
+      quantity: 1,
+    });
+  }
+
+  return (
+    <figure className="cards">
+      <div className="img-box">
+        <img
+          src={product.image}
+          alt={product.productName}
+          className="fig-img"
+        />
+      </div>
+      <div className="fig-content">
+        <h3 className="product-name">{product.productName}</h3>
+        <p className="product-description">
+          odio morbi quis commodo odio aenean sed adipiscing diam donec
+        </p>
+        <div className="cart-box">
+          <button
+            className="addToCart"
+            onClick={handleAddToCart}
+            // disabled={buttonDisabled}
+          >
+            Add To Cart
+          </button>
+          <p className="product-price">Rs.{product.price}\-</p>
+        </div>
+      </div>
+    </figure>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,331 @@
+const data = [
+  {
+    shirts: [
+      {
+        id: '2b56c7a0-94cd-4a9b-ba3d-6040769e8a01',
+        productName: 'Alpha Shirt',
+        price: '1750',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: '3a7f8be3-7a78-4f67-a4b0-ea27b54d95d0',
+        productName: 'Beta Tee',
+        price: '1296',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: '9e4e6d6e-4eb9-4c6a-8f9b-92e02d0c956c',
+        productName: 'Gamma Blouse',
+        price: '2450',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: 'c32d430a-6df5-4bb5-8f29-1e2ea4b3e29a',
+        productName: 'Delta Polo',
+        price: '2100',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: 'f3aeaa8c-5c87-456d-9b38-451f7c9e1e1e',
+        productName: 'Epsilon Dress Shirt',
+        price: '2940',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: '6bfbb7f3-1536-4e18-9ac9-b5a36e1cb708',
+        productName: 'Zeta Jersey',
+        price: '1960',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: 'a916ef4d-4a71-46ce-a7da-8484cbba0e4c',
+        productName: 'Eta Tunic',
+        price: '3510',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: 'b9358a0c-c4bf-43c3-980a-56493c92cb96',
+        productName: 'Theta Button-Up',
+        price: '6050',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: 'd7e64887-7a5a-4e44-8cd7-90b15037c1da',
+        productName: 'Iota Flannel',
+        price: '4320',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+      {
+        id: '8c0a3d74-4d3b-49db-9b01-cb9b90e4380c',
+        productName: 'Kappa Henley',
+        price: '2560',
+        quantity: '1',
+        image:
+          'https://png.pngtree.com/png-clipart/20210430/ourmid/pngtree-fashion-trend-male-model-png-image_3253771.jpg',
+      },
+    ],
+  },
+  {
+    pants: [
+      {
+        id: '6e8430f0-1dbf-4d2e-9ed9-8b0a36ca9a01',
+        productName: 'Classic Denim Jeans',
+        price: '2500',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: 'a2f0dd2f-6f33-4b15-9a37-66f16f5b06e6',
+        productName: 'Slim Fit Chinos',
+
+        price: '1800',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: 'c913cfe0-2e6c-4da2-880c-988ac2f96e04',
+        productName: 'Cargo Pants',
+        price: '2200',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: 'dfb1a2f3-9f28-4a4e-b527-854f17a19ea5',
+        productName: 'Casual Joggers',
+        price: '1500',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: '52c75d42-971f-47d3-97db-3900ebf81daa',
+        productName: 'Formal Dress Pants',
+        price: '2800',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: 'c1c9a1e9-7dd1-4a10-b2c7-27d865f8a040',
+        productName: 'Cropped Trousers',
+        price: '1900',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: 'f2b4a8c0-2c54-452a-9b0f-5c703c8db4a3',
+        productName: 'Track Pants',
+        price: '1200',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: '987e6d5c-4b3a-2f1e-1d0c-0a9b8c7f6e5d',
+        productName: 'Corduroy Trousers',
+        price: '2100',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: '1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d',
+        productName: 'Athletic Sweatpants',
+        price: '1400',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+      {
+        id: '0d9e8c7b-6a5f-4d3e-2b1c-0a9b8e7d6c5f',
+        productName: 'Pleated Trousers',
+        price: '2600',
+        quantity: '1',
+        image:
+          'https://www.pngkit.com/png/detail/73-738389_jeans-clipart-man-png-pencil-and-in-color.png',
+      },
+    ],
+  },
+  {
+    shoes: [
+      {
+        id: 'e5f1d3c2-4b5a-6d7c-8f9e-1a2b3c4d5e6f',
+        productName: 'Sneaker Black',
+        price: '2000',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '1b3c5d7e-9f0a-2c4d-6e8f-0a1b3c5d7e9f',
+        productName: 'Running Shoes',
+        price: '2500',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '2c4d6e8f-0a1b-3c5d-7e9f-4a6b8c0d2e',
+        productName: 'Classic Loafers',
+        price: '1800',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '3c5d7e9f-4a6b-8c0d-2e3f-5a6b8c0d4e',
+        productName: 'High-Top Boots',
+        price: '3000',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '4e6f8a0b-2c4d-6e8f-0a1b-3c5d7e9f4a6b',
+        productName: 'Canvas Slip-Ons',
+        price: '1500',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '5a6b8c0d-4e6f-8a0b-2c4d-6e8f0a1b3c5',
+        productName: 'Formal Oxfords',
+        price: '2800',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '6e8f0a1b-5c7d-9e0f-2a4b-6c8d0e1f3a5',
+        productName: 'Leather Sandals',
+        price: '2200',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '7a8b0c2d-4e6f-8a0b-2c4d-6e8f0a1b3c5',
+        productName: 'Athletic Sneakers',
+        price: '1700',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '8c0d2e4f-6e8f-0a1b-3c5d-7e9f4a6b8c0',
+        productName: 'Hiking Boots',
+        price: '3200',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+      {
+        id: '9e0f2a4b-6c8d-0e1f-3a5b-7c9d4e0f2a4',
+        productName: 'Slip-Resistant Work Shoes',
+        price: '2700',
+        quantity: '1',
+        image: 'https://freepngimg.com/thumb/shoes/28411-5-sneaker-clipart.png',
+      },
+    ],
+  },
+  {
+    bags: [
+      {
+        id: 'b3a1cd9e-9b74-4f2f-8d0a-15d1b2c3e4f5',
+        productName: 'Stylish Backpack',
+        price: '1800',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: 'edc2b1a0-5f7e-4d6c-8b9a-0f1e2d3c4b5a',
+        productName: 'Leather Tote Bag',
+        price: '2500',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: '6f5e4d3c-2b1a-1a0b-9c8d-7e6f5a4b3c2d',
+        productName: 'Canvas Messenger Bag',
+        price: '2200',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: 'd4c3b2a1-e0f1-9a8b-7c6d-5e4f3b2a1c0',
+        productName: 'Travel Duffel',
+        price: '2800',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: '8b9a0f1e-2d3c-4b5a-6f7e-8d9c0a1b2c',
+        productName: 'Designer Clutch',
+        price: '1500',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: '5e4f3b2a-1c0d-9e8c-7b6a-4d3e-2b1a',
+        productName: 'Laptop Briefcase',
+        price: '2100',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: '9c8d7e6f-5a4b-3c2d-1a0b-9e8c7b6a5f4e',
+        productName: 'Weekender Bag',
+        price: '1200',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: '3c2d1a0b-e8c7-6a5f-4d3e-2b1a-9c8d',
+        productName: 'Fashion Backpack',
+        price: '2100',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: 'b1a0b9c8-d7e6-5f4e-3b2a-1c0d9e8c7b6a',
+        productName: 'Vintage Satchel',
+        price: '1400',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+      {
+        id: 'f1e2d3c4-b5a6-7c8d-9e0f-1a2b3c4d5e6f',
+        productName: 'Convertible Crossbody',
+        price: '2600',
+        quantity: '1',
+        image:
+          'https://static.vecteezy.com/system/resources/previews/009/665/781/original/school-bag-for-student-or-bag-for-kid-free-png.png',
+      },
+    ],
+  },
+];
+
+export default data;
